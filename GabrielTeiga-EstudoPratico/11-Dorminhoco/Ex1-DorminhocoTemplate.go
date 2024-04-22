6b0ed5f5c0e16164654ae3dfe4d53dce1ec662ff
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-04-22 01:27:12 -0300

    Exerc11: Realizado com sucesso.

--- GabrielTeiga-EstudoPratico/11-Dorminhoco/Ex1-DorminhocoTemplate.go
+++ GabrielTeiga-EstudoPratico/11-Dorminhoco/Ex1-DorminhocoTemplate.go
@@ -8,53 +8,102 @@
 //   O ultimo leva a rolhada ...
 //   ESTE  PROGRAMA NAO FUNCIONA.    É UM RASCUNHO COM DICAS.
 
-
 package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-const NJ = 5           // numero de jogadores
-const M = 4            // numero de cartas
+const NJ = 5 // numero de jogadores
+const M = 4  // numero de cartas
 
-type carta string      // carta é um strirng
+type carta string // carta é um strirng
 
-var ch [NJ]chan carta  // NJ canais de itens tipo carta  
+var ch [NJ]chan carta          // NJ canais de itens tipo carta
+var bateu = make(chan int, NJ) // canal para bater
 
-func jogador(id int, in chan carta, out chan carta, cartasIniciais []carta) {
-	mao := cartasIniciais    // estado local - as cartas na mao do jogador
-	nroDeCartas := M         // quantas cartas ele tem 
-    cartaRecebida := " "     // carta recebida é vazia
+func jogador(id int, in chan carta, out chan carta, bateu chan int, cartasIniciais []carta) {
+	mao := cartasIniciais // estado local - as cartas na mao do jogador
+	nroDeCartas := M      // quantas cartas ele tem
+
+	if id == 0 {
+		mao = append(mao, carta("Joker"))
+		nroDeCartas++
+	}
+
+	fmt.Println("Jogador", id, "começa com", mao)
 
 	for {
-		if // tenho que jogar
-			{
-			fmt.Println(id, " joga") // escreve seu identificador
-			// escolhe alguma carta para passar adiante ...
-			// guarda carta que entrou 
-			// manda carta escolhida o proximo
-			out <- cartaParaSair        
-		} else {  
-			// ...
-			cartaRecebida := <-in   // recebe carta na entrada
-			//  ...
-			// e se alguem bate ?
+		select {
+		case <-bateu:
+			fmt.Printf("Jogador %d bateu\n", id)
+			bateu <- id
+			return
+		default:
+			if readyToWin(mao) {
+				fmt.Printf("Jogador %d ganhou\n", id)
+				fmt.Println(mao)
+				bateu <- id
+				return
+			} else if nroDeCartas > M {
+				randomIndex := rand.Intn(len(mao))
+				cartaParaSair := mao[randomIndex]
+				mao = append(mao[:randomIndex], mao[randomIndex+1:]...)
+
+				fmt.Printf("%d passou %s\n", id, cartaParaSair)
+				out <- cartaParaSair
+				nroDeCartas--
+			} else {
+				select {
+				case cartaRecebida := <-in:
+					fmt.Printf("%d recebeu %s\n", id, cartaRecebida)
+					mao = append(mao, cartaRecebida)
+					nroDeCartas++
+				case <-bateu:
+					fmt.Printf("Jogador %d bateu\n", id)
+					bateu <- id
+					return
+				}
+			}
 		}
 	}
 }
 
-func main() {
-	for i := 0; i < NJ; i++ {
-		ch[i] = make(chan struct{})
+func readyToWin(mao []carta) bool {
+	if len(mao) < 4 {
+		return false
 	}
-	// cria um baralho com NJ*M cartas
-	for i := 0; i < NJ; i++ {
-		// escolhe aleatoriamente (tira) cartas do baralho, passa cartas para jogador
-		go jogador(i, ch[i], ch[(i+1)%N], cartasEscolhidas) // cria processos conectados circularmente
-	}
-	
-	<-make(chan struct{}) // bloqueia
+	return mao[0] == mao[1] && mao[1] == mao[2] && mao[2] == mao[3]
 }
 
+func main() {
+	for i := 0; i < NJ; i++ {
+		ch[i] = make(chan carta)
+	}
 
+	// cria um baralho com NJ*M cartas
+	baralho := make([]carta, NJ*M)
+	for i := 0; i < M; i++ {
+		baralho[i] = carta("A")
+		baralho[i+4] = carta("B")
+		baralho[i+8] = carta("C")
+		baralho[i+12] = carta("D")
+		baralho[i+16] = carta("E")
+	}
+
+	// embaralha o baralho
+	rand.Shuffle(len(baralho), func(i, j int) {
+		baralho[i], baralho[j] = baralho[j], baralho[i]
+	})
+
+	// distribui as cartas para os jogadores
+	for i := 0; i < NJ; i++ {
+		cartasEscolhidas := baralho[i*M : (i+1)*M]
+		go jogador(i, ch[i], ch[(i+1)%NJ], bateu, cartasEscolhidas)
+	}
+
+	// <-make(chan struct{}) // bloqueia
+	time.Sleep(2 * time.Second)
+}
---
+++ GabrielTeiga-EstudoPratico/11-Dorminhoco/go.mod
@@ -0,0 +1,3 @@
+module github.com/gabrielteiga/canais/exerc11
+
+go 1.21.3
